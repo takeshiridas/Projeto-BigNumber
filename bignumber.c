72d10e0fa81af9d0fe1039e274956448fee624dc
Takeshi <134627945+takeshiridas@users.noreply.github.com>
2024-12-10 20:10:13 -0300

    Incializando bignumber.c

--- bignumber.c
+++ bignumber.c
@@ -1,1 +1,35 @@
-// Implementação da Interface BigNumber
+#include <stdio.h>
+#include <stdlib.h>
+#include "bignumber.h"
+
+BigNumber bignumber(void){
+	
+}
+
+void print_bignumber(BigNumber bignumber){
+	
+}
+
+BigNumber sum_bignumber(Bignumber a, BigNumber b{
+
+}
+
+BigNumber minus_bignumber(BigNumber a, BigNumber b){
+
+}
+
+BigNumber multiplication_bignumber(BigNumber a, BigNumber b){
+
+}
+
+BigNumber divide_bignumber(Bignumber a, BigNumber b){
+
+}
+
+BigNumber power_bignumber(Bignumber a, BigNumber b){
+
+}
+
+BigNumber remainder_bignumber(Bignumber a, BigNumber b){
+
+}
