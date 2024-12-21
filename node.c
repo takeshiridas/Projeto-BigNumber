9c42c71e02cddb2e56d0b511c8a23f780e2d3b0d
Takeshi <134627945+takeshiridas@users.noreply.github.com>
2024-12-21 17:31:20 -0300

    criando e implementando os node

---
+++ node.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "node.h"
+
+Node node(int data){
+    Node p = malloc(sizeof(Node));
+    p->data = data;
+    p->next = NULL;
+
+    return p;
+}
+
+void node_print_debug(Node l){
+    printf("end: %u\n", l);
+    printf("data: %d\n", l->data);
+    printf("next: %u\n", l->next);
+    printf("\n\n");
+}
+
+void node_print(Node l){
+    printf("%d, ", l->data);
+}
---
+++ node.h
@@ -0,0 +1,13 @@
+#ifndef NODE_H
+#define NODE_H
+
+typedef struct _node* Node;
+
+struct _node{
+    int data;
+    struct *_node next;
+};
+
+Node node(int data);
+void node_print(Node l);
+#endif
