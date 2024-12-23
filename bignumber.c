b72bcacadf9bd2d0789dd147406e216103f765b0
Takeshi <134627945+takeshiridas@users.noreply.github.com>
2024-12-22 21:49:59 -0300

    ajustando identação bignumber.c

--- bignumber.c
+++ bignumber.c
@@ -13,75 +13,77 @@ struct _bignumber{
 BigNumber bignumber(void){
 	BigNumber b = malloc(sizeof(struct _bignumber));
 
-    b->first = NULL;
-    b->last = NULL;
-    b->n_elements = 0;
+	b->first = NULL;
+	b->last = NULL;
+	b->n_elements = 0;
 
     return b;
 }
 
 void bignumber_push_back(BigNumber b, int data){
-    Node p = node(data);
+	Node p = node(data);
 
-    if(b->last == NULL){ //Caso a lista esteja vazio
-        b->first = p; //Atualiza o ponteiro do primeiro nó
-        b->last = p; //Atualiza o ponteiro do último nó
-    } else{
-        p->prev = b->last; //O anterior do novo nó é o primeiro nó(à esquerda)
-        b->last->next = p; //O próximo do primeiro nó é o novo nó(à direita)
-        b->last = p; //O último nó agora é o novo nó
-    }
+	if(b->last == NULL){ //Caso a lista esteja vazio
+        	b->first = p; //Atualiza o ponteiro do primeiro nó
+        	b->last = p; //Atualiza o ponteiro do último nó
+    	} else{
+        	p->prev = b->last; //O anterior do novo nó é o primeiro nó(à esquerda)
+        	b->last->next = p; //O próximo do primeiro nó é o novo nó(à direita)
+        	b->last = p; //O último nó agora é o novo nó
+    	}
 
-    b->n_elements += 1; //Aumenta o número de elementos
+    	b->n_elements += 1; //Aumenta o número de elementos
 }
 
 void bignumber_push_front(BigNumber b, int data){
-    Node p = node(data);
+	Node p = node(data);
 
-    if(b->first == NULL){ 
-        b->first = p; 
-        b->last = p;
-    } else{
-        p->next = b->first; //O próximo do novo nó é o ultimo nó(à direita)
-        b->first->prev = p; //O anterior do primeiro nó é o novo nó(à esquerda)
-        b->first = p; //O primeiro nó agora é o novo nó
-    }
+	if(b->first == NULL){ 
+        	b->first = p; 
+        	b->last = p;
+    	} else{
+        	p->next = b->first; //O próximo do novo nó é o ultimo nó(à direita)
+        	b->first->prev = p; //O anterior do primeiro nó é o novo nó(à esquerda)
+        	b->first = p; //O primeiro nó agora é o novo nó
+    	}
 
-    b->n_elements += 1;
+    	b->n_elements += 1;
 }
 
 void read_bignumber(BigNumber b){
-    char c;
-    while(scanf("%c", &c) == 1 && c != '\n'){
-        bignumber_push_back(b, c - '0');
-    }
+	char c;
+	while(scanf("%c", &c) == 1 && c != '\n'){
+        	bignumber_push_back(b, c - '0');
+    	}
 }
 
 void print_bignumber(BigNumber b){
-    Node current = b->first;
+	Node current = b->first;
 
-    for(int i = 0; i < b->n_elements && current != NULL; i++){
-        printf("%d", current->data);
-        current = current->next;
-    }
+	for(int i = 0; i < b->n_elements && current != NULL; i++){
+        	printf("%d", current->data);
+        	current = current->next;
+    	}
 }
 
-BigNumber sum_bignumber(Bignumber a, BigNumber b{
-    BigNumber result = bignumber();
+BigNumber sum_bignumber(Bignumber a, BigNumber b){
+	BigNumber result = bignumber();
 
-    Node* _a = a->last; // ponteiros para o último elemento; tail 
-    Node* _b = b->last;
-    int overflow = 0;
+    	Node* _a = a->last; // ponteiros para o último elemento; tail 
+    	Node* _b = b->last;
+    	int overflow = 0;
 
-    while (_a != NULL || _b!= NULL || overflow > 0) {
-        if (_a!=NULL)
-            digit_a = _a->data
-        else digit_a = 0;
+    	while (_a != NULL || _b!= NULL || overflow > 0) {
+        	if (_a!=NULL)
+        		digit_a = _a->data
+        	else 
+			digit_a = 0;
 
-        if (_b!=NULL)
-            digit_a = _a->data
-        else digit_a = 0;
-    }
+        	if (_b!=NULL)
+        		digit_a = _a->data
+        	else 
+			digit_a = 0;
+    	}
 }
 
 BigNumber minus_bignumber(BigNumber a, BigNumber b){
