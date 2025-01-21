08da2d59a77c818f750b8cff7feb38d9a2bfe6a4
Takeshi <134627945+takeshiridas@users.noreply.github.com>
2025-01-20 22:49:04 -0300

    Initializing client.c

--- client.c
+++ client.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
 #include "bignumber.h"
 
-int main(){
+int main() {
+    char c;
+    while (1) {
+        BigNumber a = bignumber();
+        BigNumber b = bignumber();
 
+        // Lê os dois números
+        read_bignumber(a);
+        read_bignumber(b);
 
-	return 0;
+        // Lê o operador (ignora espaços ou quebras de linha antes)
+        if (scanf(" %c", &c) != 1) { 
+            free_bignumber(a);
+            free_bignumber(b);
+            break; // Sai do loop se atingir EOF
+        }
+
+		BigNumber result = NULL;
+
+        if (c == '+') {
+            result = sum_bignumber(a, b);
+        } else if (c == '-') {
+            result = minus_bignumber(a, b);
+        }
+
+        if (result != NULL) {
+            print_bignumber(result);
+            free_bignumber(result);
+        }
+
+        // Ignora possíveis novas linhas ou espaços extras após o operador
+        while ((c = getchar()) != EOF && c != '\n') {
+            // Consumir caracteres extra até a próxima linha
+        }
+
+        free_bignumber(a);
+        free_bignumber(b);
+    }
+    return 0;
 }
